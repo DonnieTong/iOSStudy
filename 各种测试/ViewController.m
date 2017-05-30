23fc79c01544166fc4935a8819f7086b77252de9
DonnieTong <Donnie@DonniedeMacBook-Pro.local>
2017-05-30 21:02:43 +0800

    01

--- 各种测试/ViewController.m
+++ 各种测试/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UIView *testView;
 
 @end
 
@@ -16,9 +19,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self.view setBackgroundColor:[UIColor whiteColor]];
+//    [self.view setBackgroundColor:[UIColor.whiteColor colorWithAlphaComponent:0.5f]];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, CGRectGetHeight(self.view.bounds) - 100, CGRectGetWidth(self.view.bounds), 100)];
+//    [view setBackgroundColor:[UIColor whiteColor]];
+    [view setBackgroundColor:[UIColor colorWithWhite:0.9f alpha:0.5f]];
+//    [self.view addSubview:view];
+//    [view setAlpha:0.2];
+//    [view.layer setOpacity:0.5f];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, view.frame.size.width, 40)];
+    label.textAlignment = NSTextAlignmentCenter;
+    [label setBackgroundColor: [UIColor lightGrayColor]];
+    [label setTextColor:[UIColor blackColor]];
+    label.text = @"各种测试";
+    [view addSubview:label];
+    [_button.titleLabel setTextAlignment:NSTextAlignmentLeft];
 }
-
+- (IBAction)pressed:(UIButton *)sender {
+    if (_testView.hidden) {
+//        [self.view setBackgroundColor:[UIColor colorWithWhite:1.0f alpha:0.9f]];
+        [self.view setBackgroundColor:[UIColor.whiteColor colorWithAlphaComponent:0.9f]];
+        _testView.hidden = NO;
+    }else {
+        _testView.hidden = YES;
+        [self.view setBackgroundColor:[UIColor whiteColor]];
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
